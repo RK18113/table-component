c394b73d21b249842ea92fb8dcf8ad8cc3d91578
RK18113 <ramakrishnanilanghovan@gmail.com>
2025-02-10 12:44:52 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import TopicDescription from "./components/TopicDescription";
+import { useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
+
+function App() {
+  const values = {
+    topic1: "description1",
+    topic2: "description2",
+    topic3: "description3",
+    topic4: "description4",
+    topic5: "description5",
+    topic6: "description6",
+    topic7: "description7",
+    topic8: "description8",
+  };
+
+  return (
+    <>
+      <div className="bg-black h-screen w-screen"></div>
+      <div className="bg-white h-[1px] w-screen"></div>
+
+      <div className="bg-black min-h-screen w-screen flex flex-nowrap justify-center items-start px-10 py-2 gap-10">
+        <div className="w-[40%]">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdBWB76EZKUgHdARYa-XNyIzoiJiUiyKiFrg&s"
+            alt=""
+            className="w-full h-[60vh] rounded-lg shadow-lg"
+          />
+        </div>
+        <TopicDescription values={values} />
+      </div>
+
+      <div className="bg-black h-screen w-screen"></div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TopicDescription.jsx
@@ -0,0 +1,93 @@
+import React, { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+function TopicDescription({ values }) {
+  const containerRef = useRef(null);
+  const rowRefs = useRef([]);
+  const lineRefs = useRef([]);
+  const valueRefs = useRef([]);
+
+  useGSAP(() => {
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "top center",
+        toggleActions: "play reverse play reverse",
+        markers: true,
+      },
+    });
+
+    timeline.fromTo(
+      lineRefs.current,
+      {
+        scaleX: 0,
+        transformOrigin: "left",
+      },
+      {
+        scaleX: 1,
+        duration: 0.5,
+      }
+    );
+
+    timeline.fromTo(
+      rowRefs.current,
+      {
+        x: "-100%",
+      },
+      {
+        x: 0,
+        duration: 0.3,
+      },
+    );
+
+    timeline.fromTo(
+      valueRefs.current,
+      {
+        x: "-100%",
+      },
+      {
+        x: 0,
+        duration: 0.3,
+      },
+    );
+  }, []);
+
+  return (
+    <div ref={containerRef} className="text-white w-[60%]">
+      {Object.entries(values).map(([key, value], index) => (
+        <div key={key} className="relative">
+          <div className="flex flex-wrap justify-start items-center">
+            
+            <div className="overflow-hidden">
+              <div
+                ref={(el) => (rowRefs.current[index] = el)}
+                className="text-sm text-green-500 tracking-wider uppercase"
+              >
+                {key}
+              </div>
+            </div>
+
+            <div className="overflow-hidden ml-[25vw]">
+              <div
+                ref={(el) => (valueRefs.current[index] = el)}
+                className="text-sm tracking-wider uppercase"
+              >
+                {value}
+              </div>
+            </div>
+
+            <div className="flex-grow w-full py-3">
+              <div
+                ref={(el) => (lineRefs.current[index] = el)}
+                className="h-[1px] w-full bg-white"
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TopicDescription;
